2fd690f38f72458004d28d2556e92bf2b5188914
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-11-13 20:47:08 +0100

    Add posts to SQL table

--- c98.sql
+++ c98.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2025 at 09:07 PM
+-- Generation Time: Nov 13, 2025 at 08:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -26,10 +26,22 @@ USE `c98`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `description` varchar(1024) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `fs`
 --
 
-DROP TABLE IF EXISTS `fs`;
 CREATE TABLE IF NOT EXISTS `fs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `path` varchar(1024) NOT NULL,
@@ -44,10 +56,24 @@ CREATE TABLE IF NOT EXISTS `fs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `likes`
+--
+
+CREATE TABLE IF NOT EXISTS `likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` int(11) NOT NULL,
+  `post` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `likes__owner` (`owner`),
+  KEY `likes__post` (`post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `links`
 --
 
-DROP TABLE IF EXISTS `links`;
 CREATE TABLE IF NOT EXISTS `links` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner` int(11) NOT NULL,
@@ -63,10 +89,25 @@ CREATE TABLE IF NOT EXISTS `links` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic` int(11) NOT NULL,
+  `owner` int(11) NOT NULL,
+  `content` varchar(4096) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `posts__owner` (`owner`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tokens`
 --
 
-DROP TABLE IF EXISTS `tokens`;
 CREATE TABLE IF NOT EXISTS `tokens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner` int(11) NOT NULL,
@@ -78,10 +119,27 @@ CREATE TABLE IF NOT EXISTS `tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `topic`
+--
+
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `mainPost` int(11) NOT NULL,
+  `category` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `topic__owner` (`owner`),
+  KEY `topic__mainPost` (`mainPost`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) NOT NULL,
@@ -102,16 +160,36 @@ ALTER TABLE `fs`
   ADD CONSTRAINT `owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
 
 --
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes__owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `likes__post` FOREIGN KEY (`post`) REFERENCES `posts` (`id`);
+
+--
 -- Constraints for table `links`
 --
 ALTER TABLE `links`
   ADD CONSTRAINT `links__owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
 
 --
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts__owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
+
+--
 -- Constraints for table `tokens`
 --
 ALTER TABLE `tokens`
   ADD CONSTRAINT `tokens__owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `topic`
+--
+ALTER TABLE `topic`
+  ADD CONSTRAINT `topic__mainPost` FOREIGN KEY (`mainPost`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `topic__owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
